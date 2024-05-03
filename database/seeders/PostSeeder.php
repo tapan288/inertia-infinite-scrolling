e4c7f7d5051b39953272802cf8b196f566977c65
Tapan Sharma <tapansharma69@gmail.com>
2024-05-03 11:32:43 +0545

    define model/migration/factory/seeder and seed the data

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::factory()->times(100)
+            ->state(
+                new Sequence(
+                    fn(Sequence $sequence) => [
+                        'created_at' => now()->addDays($sequence->index)
+                    ]
+                )
+            )
+            ->create();
+    }
+}
