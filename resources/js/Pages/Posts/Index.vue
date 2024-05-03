24b8289782bbadbe0c02c493f8716a31154444e0
Tapan Sharma <tapansharma69@gmail.com>
2024-05-03 17:00:51 +0545

    fix remaining issues

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -15,34 +15,34 @@ const postsData = ref(props.posts.data);
 const path = ref(props.posts.meta.path);
 const next_cursor = ref(props.posts.meta.next_cursor);
 
-useIntersectionObserver(lastElement, ([{ isIntersecting }]) => {
-    if (!isIntersecting) {
-        return;
-    }
+const { stop } = useIntersectionObserver(
+    lastElement,
+    ([{ isIntersecting }]) => {
+        if (!isIntersecting) {
+            return;
+        }
 
-    // Fetch more posts
-    axios.get(`${path.value}?cursor=${next_cursor.value}`).then((response) => {
-        postsData.value = [...postsData.value, ...response.data.data];
-        next_cursor.value = response.data.meta.next_cursor;
-    });
-    // router.reload({
-    //     data: {
-    //         page: props.posts.meta.current_page + 1,
-    //     },
-    //     onSuccess: () => {
-    //         postsData.value = [...postsData.value, ...props.posts.data];
-    //     },
-    // });
-});
+        // Fetch more posts
+        axios
+            .get(`${path.value}?cursor=${next_cursor.value}`)
+            .then((response) => {
+                postsData.value = [...postsData.value, ...response.data.data];
+                next_cursor.value = response.data.meta.next_cursor;
+
+                if (!next_cursor.value) {
+                    stop();
+                }
+            });
+    }
+);
 </script>
 
 <template>
-    <!-- {{ props.posts.meta }} -->
     <div class="max-w-2xl mx-auto my-12 space-y-12">
         <div v-for="post in postsData" :key="post.id">
             <h1 class="font-bold text-3xl">{{ post.id }}: {{ post.title }}</h1>
             <p class="mt-2 text-lg">{{ post.teaser }}</p>
         </div>
-        <div ref="lastElement"></div>
+        <div ref="lastElement" class="-translate-y-32"></div>
     </div>
 </template>
