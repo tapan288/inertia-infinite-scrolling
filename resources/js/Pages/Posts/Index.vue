2da367bffdca2c6b6418beb128c7309590875c4b
Tapan Sharma <tapansharma69@gmail.com>
2024-05-03 12:59:51 +0545

    start implementing infinite scrolling

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref } from "vue";
 import { useIntersectionObserver } from "@vueuse/core";
+import { router, usePage } from "@inertiajs/vue3";
 
-defineProps({
+const props = defineProps({
     posts: {
         type: Object,
         required: true,
@@ -10,17 +11,29 @@ defineProps({
 });
 
 const lastElement = ref(null);
+const postsData = ref(props.posts.data);
 
 useIntersectionObserver(lastElement, ([{ isIntersecting }]) => {
     if (!isIntersecting) {
         return;
     }
+
+    // Fetch more posts
+    router.reload({
+        data: {
+            page: props.posts.meta.current_page + 1,
+        },
+        onSuccess: () => {
+            postsData.value = [...postsData.value, ...props.posts.data];
+        },
+    });
 });
 </script>
 
 <template>
+    <!-- {{ posts }} -->
     <div class="max-w-2xl mx-auto my-12 space-y-12">
-        <div v-for="post in posts.data" :key="post.id">
+        <div v-for="post in postsData" :key="post.id">
             <h1 class="font-bold text-3xl">{{ post.id }}: {{ post.title }}</h1>
             <p class="mt-2 text-lg">{{ post.teaser }}</p>
         </div>
